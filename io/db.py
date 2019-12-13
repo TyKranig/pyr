d44500f3651b3940e00b4bb8be06edc84d074a02
Tyler Kranig <tylerkranig@gmail.com>
2019-12-12 23:55:11 -0600

    formatting db as an object

--- io/db.py
+++ io/db.py
@@ -8,6 +8,23 @@ def clearCol(col):
   x = col.delete_many({})
   print(x.deleted_count, " listings deleted")
 
+class DataWriter():
+  def __init__(self, box):
+    self.client = pymongo.MongoClient("mongodb://192.168.1.2:27017/")
+    self.db = self.client["database"]
+    self.collection = self.db[box]
+
+  def clearData(self, box):
+    clearCol(self.collection)
+
+  def writeData(self, box, data):
+    self.collection.insert_many(data)
+  
+  def printData(self, box):
+    for game in self.collection.find().sort("duration", -1).limit(10):
+      print(datetime.timedelta(seconds=game["duration"]), game["match_id"])
+
+
 # IDs for seasons 1 2 and 3 respectively
 leagues = [10824,11086,11336]
 # games to skip that were broken
@@ -15,21 +32,13 @@ gamesSkipped = [5036395844]
 
 # sets = []
 # api = ApiCall()
-# session = requests.Session()
 # listings = api.getLeague(session, league_id=11336)['result']['matches']
 # for g in listings:
 #   if g["match_id"] not in gamesSkipped:
 #     sets.append(api.getMatch(session, match_id=g["match_id"])["result"])
 
-client = pymongo.MongoClient("mongodb://192.168.1.2:27017/")
-db = client["database"]
-collection = db["games"]
+# for game in collection.find().sort("duration", -1).limit(10):
+#   print(datetime.timedelta(seconds=game["duration"]), game["match_id"])
 
-# clearCol(collection)
-# x = collection.insert_many(sets)
-
-for game in collection.find().sort("duration", -1).limit(10):
-  print(datetime.timedelta(seconds=game["duration"]), game["match_id"])
-
-for game in collection.find().sort("duration", 1).limit(10):
-  print(datetime.timedelta(seconds=game["duration"]), game["match_id"])
+# for game in collection.find().sort("duration", 1).limit(10):
+#   print(datetime.timedelta(seconds=game["duration"]), game["match_id"])
