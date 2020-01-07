3f2bbeb4020ef04552fc4e4c6d11a4afec6ed3aa
Tyler Kranig <tylerkranig@gmail.com>
2020-01-06 22:06:24 -0600

    attempting to load captains Still thinking about how to do it

--- io/parseCaptains.py
+++ io/parseCaptains.py
@@ -1,9 +1,12 @@
 from dataObjects import Captain
+from dao import DataWriter
 import io
 import os
 
 def loadCaptains():
     seasons = []
+
+    input = DataWriter("Captains")
 
     with open(os.getcwd() + r"/io/static/.env", "r") as file:
         captains = {}
