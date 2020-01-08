532160925ff9f86eb06cbf931748ffbeffe0d36f
Tyler Kranig <tylerkranig@gmail.com>
2020-01-07 23:09:33 -0600

    started parsing captain file

--- io/parseCaptains.py
+++ io/parseCaptains.py
@@ -4,12 +4,19 @@ import io
 import os
 
 def loadCaptains():
-    seasons = []
+  seasons = []
 
-    input = DataWriter("Captains")
+  input = DataWriter("Captains")
 
-    with open(os.getcwd() + r"/io/static/.env", "r") as file:
-        captains = {}
-        for line in file:
-            if "Season" in line:
-                break
+  with open(os.getcwd() + r"/io/static/.env", "r") as file:
+    captains = []
+    for line in file:
+      if "Season" in line:
+        seasons.append(captains.copy())
+        captains = []
+      else:
+        arr = line.split("\t")
+        captains.append({
+          "name": arr[0],
+          "playerId": arr[1].split("https://www.dotabuff.com/players/")[1]
+        })
