ea3848ccc5a938f9a7f5ba21f50aed3d4b5bbde8
Tyler Kranig <tylerkranig@gmail.com>
2020-01-09 20:02:05 -0600

    parsing captains into array

--- io/parseCaptains.py
+++ io/parseCaptains.py
@@ -8,15 +8,22 @@ def loadCaptains():
 
   input = DataWriter("Captains")
 
-  with open(os.getcwd() + r"/io/static/.env", "r") as file:
+  with open(os.getcwd() + r"\io\static\Captains.txt", "r") as file:
+    
     captains = []
-    for line in file:
+    for index, line in enumerate(file):
       if "Season" in line:
-        seasons.append(captains.copy())
-        captains = []
+        if len(captains) > 0:
+          seasons.append(captains.copy())
+          captains = []
       else:
-        arr = line.split("\t")
+        arr = line.split("https://www.dotabuff.com/players/")
+        print(arr)
         captains.append({
-          "name": arr[0],
-          "playerId": arr[1].split("https://www.dotabuff.com/players/")[1]
+          "name": arr[0].strip(),
+          "playerId": int(arr[1].strip())
         })
+  return seasons
+
+if __name__ == "__main__":
+    print(loadCaptains())
