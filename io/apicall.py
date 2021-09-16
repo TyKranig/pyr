a63ec82e1162d93fd87752b7afa7ff6dc78a6f27
Tyler Kranig <tylerkranig@gmail.com>
2021-09-16 14:11:08 -0500

    F

--- io/apicall.py
+++ io/apicall.py
@@ -18,6 +18,7 @@ def get_key():
 class ApiCall:
     def __init__(self):
         self.key = get_key()
+        print(self.key)
         self.session = requests.session()
 
     def getLeague(self, **kwargs):
